eea292739eacc6b25dc119cbb3aa682568016524
Edward Carrillo <eccarrilloe@gmail.com>
2017-10-05 23:23:51 -0500

    feat(app): app components added to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule
